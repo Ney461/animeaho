09d7a197ba8b0a0104e67f34800ab02d45840969
Ney461 <neymd002@gmail.com>
2026-09-15 21:21:20 -0500

    feat: create environment.example with apiUrl configuration

---
+++ src/environments/environmets.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+
+  // URL del backend
+  // Opción 1: si subes el backend a un servidor (Heroku, Vercel, VPS, etc.)
+  // apiUrl: 'https://tu-servidor.com'
+
+  // Opción 2: si corres el backend localmente
+  // apiUrl: 'http://localhost:8000'
+};
